d80a17fb4d42150b4a3744de653d53b8b3b119ce
nathansmith-hub <nathanhsmith@live.com>
2025-09-15 16:30:50 +1200

    added player information

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    delta_time = 0
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
@@ -13,10 +19,11 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         #limit FPS to 60
-        delta_time = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
