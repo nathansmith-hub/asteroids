14c0bbe05b90627238328931b933a398ef429512
nathansmith-hub <nathanhsmith@live.com>
2025-09-15 15:49:56 +1200

    FPS limit

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
-
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0, 0, 0)
+    clock = pygame.time.Clock()
+    delta_time = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
+        
+        screen.fill("black")
         pygame.display.flip()
+
+        #limit FPS to 60
+        delta_time = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
