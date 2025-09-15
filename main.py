0db6d0c266d9cefcbd5e32909b84f05dcf989595
nathansmith-hub <nathanhsmith@live.com>
2025-09-15 16:58:03 +1200

    updated player movements

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
-        player.update(dt)
         pygame.display.flip()
 
         #limit FPS to 60
