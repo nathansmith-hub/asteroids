c43c2f4ea2c59570314f15925b59a0575ccad54e
nathansmith-hub <nathanhsmith@live.com>
2025-09-16 17:31:31 +1200

    creating asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,13 +13,18 @@ def main():
     #use groups to manage objects
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    #asteroid_field = AsteroidField()
     
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
--- player.py
+++ player.py
@@ -5,9 +5,6 @@ from circleshape import CircleShape
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.x = x
-        self.y = y
-        self.radius = PLAYER_RADIUS
         self.rotation = 0
 
     def triangle(self):
